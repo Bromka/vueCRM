3a4dceeda24b80d764490f8b1839841803ab5e45
Bromka <efimov.iliya@gmail.com>
2020-10-01 15:16:01 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+  }
+})
+export default class App extends Vue {
+    mounted () {
+        console.log (this.$router)
+    }
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
